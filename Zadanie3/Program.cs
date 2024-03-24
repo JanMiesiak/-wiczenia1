cd10678cc5e58e1fbc0ea5df35f8b2aee9059b4a
JanMiesiak <s20867@pjwstk.edu.pl>
2024-03-24 22:28:55 +0100

    feature-average-commit

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,15 @@
+﻿static double calculateAverage(int []arr)
+{
+    double avg = 0;
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            avg+=arr[i];
+            
+        }
+        avg = avg/arr.Length;
+        return avg;
+    }
+
+//example use
+int[] tmp = [1,2,3,4];
+Console.WriteLine(calculateAverage(tmp));
